a05b243d467b0ff4c68816aaeb0b88055605da65
Robson456 <przepiorkarobin@gmail.com>
2020-02-04 12:33:14 +0100

    first commit

---
+++ src/components/navbar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function navbar() {
+    return (
+        <div>
+            <div>
+                <h1>Wotered</h1>
+            </div>
+            <div className="links">
+                <ul>
+                    <li>Home</li>
+                    <li>Home</li>
+                    <li>Home</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
